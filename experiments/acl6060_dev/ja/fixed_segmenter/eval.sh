2af1d78863316ffb56b267ed47ffd4846e1f7f5d
Peter Polak <polak@ufal.mff.cuni.cz>
2025-03-11 15:18:14 +0100

    ja zh with fixed segmenter

---
+++ experiments/acl6060_dev/ja/fixed_segmenter/eval.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for log_path in final/segment-length_8.0_step-length_*/instances.log; do
+    python3 ${REPO_ROOT_PATH}/tools/stream_laal.py --simuleval-instances $log_path  \
+            --reference ${REPO_ROOT_PATH}/data/acl6060_dev/ja/tgt_segments.txt \
+            --audio-yaml ${REPO_ROOT_PATH}/data/acl6060.yaml \
+            --sacrebleu-tokenizer ja-mecab \
+            --latency-unit char > $(dirname $log_path)/scores.resegmented.tsv
+done
---
+++ experiments/acl6060_dev/zh/fixed_segmenter/README.md
@@ -0,0 +1,23 @@
+# SeamlessM4T with Fixed Segmenter
+
+## Run Experiments
+
+Set enviroment variables:
+```
+export REPO_ROOT_PATH=
+export PYTHON_ENV_DIR=
+```
+
+Run the final runs in simultaneous mode with segment length of 8 seconds (we use the same segment length as the data and model are the same):
+```
+bash final.sh
+```
+
+Run evaluation script:
+```
+bash eval.sh
+```
+
+## Final Results
+
+TBD
