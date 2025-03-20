3def45d5450baaa82fe1811153d65e1567448a1a
Peter Polak <polak@ufal.mff.cuni.cz>
2025-03-20 10:54:07 +0100

    en->ja vad

---
+++ experiments/acl6060_dev/ja/vad_segmenter/run_all.sh
@@ -0,0 +1,10 @@
+for max_unvoiced_length in 0.1
+do
+    for voice_threshold in 0.1 
+    do
+        for step_length in 0.5 0.75 1.0 1.25 1.5 1.75 2.0
+        do
+            sbatch run_simuleval.sh $max_unvoiced_length $voice_threshold $step_length
+        done
+    done
+done
